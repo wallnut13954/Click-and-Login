af1836f81349749e1122892f394645102d58057c
wallnut13954 <156524983+wallnut13954@users.noreply.github.com>
2024-01-20 12:42:36 +0000

    Changed to extract a number

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ def get_driver():
   driver.get("http://automated.pythonanywhere.com")
   return driver
 
+def cleantext(text):
+  output=float(text.split(": ")[1])
+  return output
+
 def main():
   driver=get_driver()
   time.sleep(2)
   element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
-  return element.text
+  return cleantext(element.text)
 print(main())
