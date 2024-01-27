dfe47a32831a78eae1d6f660052e827015e86b2a
wallnut13954 <156524983+wallnut13954@users.noreply.github.com>
2024-01-27 16:52:46 +0000

    A

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import selenium
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 def get_driver():
   # Set options to make browsing easier
   options = webdriver.ChromeOptions()
@@ -12,16 +13,23 @@ def get_driver():
   options.add_argument("disable-blink-features=AutomationControlled")
 
   driver = webdriver.Chrome(options=options)
-  driver.get("http://automated.pythonanywhere.com")
+  driver.get("https://automated.pythonanywhere.com/login/")
   return driver
 
 def cleantext(text):
-  output=float(text.split(": ")[1])
+  output=text.split("Belgium, ")[1]
   return output
 
 def main():
   driver=get_driver()
   time.sleep(2)
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
-  return cleantext(element.text)
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  time.sleep(2)
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  time.sleep(2)
+  print(driver.current_url)
+  
+
 print(main())
